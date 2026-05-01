3720eeaea436e33aa447077b6addf2467b937e5c
Ashish Sharma <ashu11vats@gmail.com>
2026-05-01 19:12:31 +0530

    Updated main.py

--- app/main.py
+++ app/main.py
@@ -3,15 +3,29 @@ from app.core.config import settings
 
 from app.api.routes import auth, note
 
+# Import Base and engine
+from app.db.base import Base
+from app.db.session import engine
+
+# Import models so SQLAlchemy can detect them
+from app.models.user import User
+from app.models.note import Note
+
+
 app = FastAPI(
     title=settings.APP_NAME,
     debug=settings.DEBUG
 )
 
 
+# Create tables on startup
+Base.metadata.create_all(bind=engine)
+
+
 @app.get("/")
 def root():
     return {"message": "Notes Backend is running"}
+
 
 app.include_router(auth.router)
 app.include_router(note.router)
